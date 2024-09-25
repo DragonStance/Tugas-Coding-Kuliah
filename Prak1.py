0a55553b441ad9a48bf44d8a54eadde1887b0f86
DragonStance <ridhwanfiamanila@gmail.com>
2024-09-25 10:14:50 +0700

    Add files via upload

---
+++ Prak1.py
@@ -0,0 +1,7 @@
+nama = input("Nama kamu siapa?: ")
+asal = input(f"Darimana asal kamu {nama}? ")
+sekolah = input(f"Sebelumnya bersekolah dimana? ")
+
+print(f"Hallo selamat siang {nama}.")
+print(f"Wah dari {asal}.")
+print(f"Pasti sangat menyenangkan sekolah di {sekolah}.")
