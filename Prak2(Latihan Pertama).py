05a3c8c77e0c94c6905841729ae6a3a07d761e5a
DragonStance <ridhwanfiamanila@gmail.com>
2024-10-01 14:44:39 +0700

    Add files via upload

---
+++ Prak2(Latihan Pertama).py
@@ -0,0 +1,11 @@
+import math
+a = float(input("Masukkan nilai A: "))
+b = float(input("Masukkan nilai B: "))
+
+print("Hasil dari penjumlahan A+B adalah:", (a + b))
+print("Selisih antara A dan B adalah:", (a - b))
+print("Jumlah A di kali B adalah:", (a * b))
+print("Jumlah sisa pembagian dari hasil A dibagi B adalah:", (a % b))
+print("Jumlah A dibagi B adalah:", (a / b))
+print(f"Hasil dari Log(A) adalah:", math.log(a))
+print("Hasil A pangkat B adalah:", (a ** b))
